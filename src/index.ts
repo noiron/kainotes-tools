27d470b1ac1257bbe419529e6896cbb69a4a66d5
wu kai <wukai2048@gmail.com>
2023-02-08 12:21:23 +0800

    feat: add purifyTag

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import * as path from 'path';
 
-export function foo() {
-  console.log('foo');
-}
-
 /**
  * 判断一个文件是否为 markdown 文件
  */
 export const isMarkdownFile = (filePath: string) => {
   const extname = path.extname(filePath).toLowerCase();
   return extname === '.md' || extname === '.markdown';
+};
+
+/**
+ * 在标签文字的获取过程中，可能会带上开头的 #，统一用这个函数去处理
+ */
+export const purifyTag = (tag: string) => {
+  if (typeof tag !== 'string' || tag.length === 0) return '';
+  if (tag[0] !== '#') return tag;
+  return tag.slice(1);
 };
